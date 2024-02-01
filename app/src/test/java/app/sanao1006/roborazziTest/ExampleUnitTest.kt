cdc322d862e85754cb11c940f8b88f3e05c756ec
sanao1006 <naosak1006@gmail.com>
2024-02-01 17:07:58 +0900

    init

---
+++ app/src/test/java/app/sanao1006/roborazziTest/ExampleUnitTest.kt
@@ -0,0 +1,40 @@
+package app.sanao1006.roborazziTest
+
+import androidx.compose.ui.test.isRoot
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.captureRoboImage
+import org.junit.Test
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+@RunWith(RobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(qualifiers = RobolectricDeviceQualifiers.Pixel7Pro)
+class ExampleUnitTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun captureRoboImageSample() {
+        ActivityScenario.launch(MainActivity::class.java)
+
+        onView(ViewMatchers.isRoot())
+            .captureRoboImage()
+
+        composeTestRule.onNode(isRoot())
+            .captureRoboImage("build/compose.png")
+    }
+}
