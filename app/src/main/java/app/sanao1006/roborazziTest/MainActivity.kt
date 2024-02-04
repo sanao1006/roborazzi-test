d474193d16bfecf95646c43b5578c2c19f451ffb
sanao1006 <naosak1006@gmail.com>
2024-02-04 23:15:10 +0900

    fix

--- app/src/main/java/app/sanao1006/roborazziTest/MainActivity.kt
+++ app/src/main/java/app/sanao1006/roborazziTest/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
-//                    Greeting("iOS")
+                    Greeting("iOS")
                 }
             }
         }
